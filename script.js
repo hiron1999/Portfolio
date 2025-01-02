1000a9eab52db8e4067e5b0ce0f209b0a8ecc6d9
hiron1999 <hironsaha0@gmail.com>
2025-01-02 18:56:40 +0530

    added content

---
+++ script.js
@@ -0,0 +1,121 @@
+
+// get content
+async function fetchContent() {
+  return await fetch("content.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // console.log(data);
+            return data;
+        }).catch((error) => {
+            console.error('Error:', error);
+        });
+}
+//loading
+fetchContent().then((data) => {
+  const about = document.getElementById('about');
+
+  const intro = document.getElementById('intro') ;
+  const intro_data = `Hey there! I'm ${data.name}`;
+  typEffect(intro,intro_data , 50);
+  //display about
+  about.getElementsByTagName('p')[0].innerText = data.about;
+  //display skills
+  const skills = document.getElementById('skills');
+  skills.innerHTML = '';
+
+ data.skills.forEach((skill) => {
+  const skillElement = document.createElement('div');
+  skillElement.classList.add('skill');
+  skillElement.innerHTML = `
+    <p>${skill}</p>
+  `;
+  skills.appendChild(skillElement);
+ 
+});
+
+//display experience
+const experience = document.getElementById('expriences');
+experience.innerHTML = '';
+data.expriences.forEach((exp) => {
+  const expElement = document.createElement('div');
+  expElement.classList.add('exprience');
+  expElement.innerHTML = `
+    <div > 
+      <h3>${exp.title}</h3>
+    </div>
+    <div class="body">
+      <p>${exp.company} </p>
+      <p>${exp.description}</p>
+    </div>
+    `;
+  experience.appendChild(expElement);
+});
+
+
+  //display projects
+  const projects = document.getElementById('projects');
+  projects.innerHTML = '';
+  data.projects.forEach((project) => {
+    const projectElement = document.createElement('card');
+    projectElement.classList.add('project');
+    projectElement.innerHTML = `
+      <card.header> 
+      <h3>${project.name}</h3>
+      </card.header>
+      <card.body>
+      <p>${project.description}</p>
+      <div class="tech"> ${project.technologies.map(element => {
+        console.log(element);
+        return `<span>${element}</span>`;
+      }).join('')}</div>
+      </card.body>
+      <card.footer>
+      
+        <a class="linkButton" href="${project.link}" target="_blank"> 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>      
+        source</a>
+
+        <a class="linkButton" href="${project.link}" target="_blank">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z"/></svg>
+        website</a>
+      
+      </card.footer>
+    `;
+    projects.appendChild(projectElement);
+  });
+
+  //display education
+  const education = document.getElementById('educations');
+  education.innerHTML = '';
+  data.education.forEach((edu) => {
+    const eduElement = document.createElement('card');
+    eduElement.classList.add('education');
+    eduElement.innerHTML = `
+      <card.header> 
+      <h3>${edu.degree}</h3>
+      </card.header>
+      <card.body>
+      <span>${edu.institution} </span>
+      <span style="float: right;">${edu.years}</span>
+      </card.body>
+      <card.footer>
+      <p>${edu.score}</p>
+      </card.footer>
+    `;
+    education.appendChild(eduElement);
+  });
+
+});
+
+//typeing effect
+function typEffect(element, text, speed) {
+  let i = 0;
+  const type = () => {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(type, speed);
+    }
+  };
+  type();
+}
