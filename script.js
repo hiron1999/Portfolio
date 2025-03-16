d21cc9f59d719ede3cac25b5cdc19460c13597a3
hiron1999 <hironsaha0@gmail.com>
2025-03-16 19:56:39 +0530

    update styleing

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// import { Packery } from "./packery.pkgd.js";
+
+const sections = document.querySelectorAll("section");
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        }, { threshold: 0.2 });
+
+        sections.forEach(section => observer.observe(section));
 
 // get content
 async function fetchContent() {
@@ -10,6 +23,7 @@ async function fetchContent() {
             console.error('Error:', error);
         });
 }
+
 //loading
 fetchContent().then((data) => {
   const about = document.getElementById('about');
@@ -19,6 +33,50 @@ fetchContent().then((data) => {
   typEffect(intro,intro_data , 50);
   //display about
   about.getElementsByTagName('p')[0].innerText = data.about;
+
+  //display Services
+  const items= [];
+  const services = document.getElementById('services');
+  const slide = document.createElement('div');
+  slide.setAttribute("class","main-carousel");
+  services.innerHTML='';
+  data.services.forEach((service)=>{
+    const scard= document.createElement('div');
+    scard.setAttribute("class","scard carousel-cell");
+    async function readsvg(path) {
+      return await fetch(path)
+      .then(response => response.text())
+      .catch(error => console.error("Error loading SVG:", error));
+    }
+    readsvg(service.logo).then((svg)=>{
+      scard.innerHTML=
+      `${svg}
+      <h2>${service.name}</h2>
+      <p>${service.desc}</p>`;
+    });
+   
+    items.push(scard);
+    slide.appendChild(scard);
+  });
+  var prev_itm = slide.firstChild;
+  // slide.removeChild(prev_itm);
+  services.appendChild(slide);
+
+  // setInterval(()=>{
+  //   const cur_itm= slide.firstChild;
+  //   slide.removeChild(cur_itm);
+  //   slide.append(prev_itm);
+  //   prev_itm=cur_itm;
+    
+  // },2000);
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem,{
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: true,
+    wrapAround: true
+  });
+  
   //display skills
   const skills = document.getElementById('skills');
   skills.innerHTML = '';
@@ -59,17 +117,18 @@ data.expriences.forEach((exp) => {
     const projectElement = document.createElement('card');
     projectElement.classList.add('project');
     projectElement.innerHTML = `
-      <card.header> 
-      <h3>${project.name}</h3>
+      <card.header style="grid-area:header ;height:auto" > 
+      <h3 >${project.name}</h3>
       </card.header>
-      <card.body>
+      <card.body  style="grid-area:body ;height:auto">
       <p>${project.description}</p>
       <div class="tech"> ${project.technologies.map(element => {
-        console.log(element);
+        // console.log(element);
         return `<span>${element}</span>`;
       }).join('')}</div>
+      <img class="img" src=${project.image}>
       </card.body>
-      <card.footer>
+      <card.footer style="grid-area:footer ;height:auto">
       
         <a class="linkButton" href="${project.link}" target="_blank"> 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>      
@@ -83,6 +142,8 @@ data.expriences.forEach((exp) => {
     `;
     projects.appendChild(projectElement);
   });
+
+
 
   //display education
   const education = document.getElementById('educations');
@@ -104,6 +165,26 @@ data.expriences.forEach((exp) => {
     education.appendChild(eduElement);
   });
 
+  var pckry, resuffl;
+  resuffl=()=>{pckry.layout();}
+    const certficates=document.getElementById("certificates");
+    certficates.innerHTML='';
+    const cert_containr=document.createElement("div");
+    cert_containr.classList.add('cert_cnr');
+    data.certification.forEach((cc)=>{
+      const cert = document.createElement("div");
+      cert.classList.add('cert');
+      cert.innerHTML=`<img src="${cc.cover}" alt="certificate"><span ><a href = "${cc.view}" target="_blank">view</br> Credential</a></span>`;
+      cert.addEventListener("mouseover",resuffl);
+      cert_containr.appendChild(cert);
+    });
+    certficates.appendChild(cert_containr);
+    
+    pckry=new Packery(cert_containr,{
+      percentPosition: true,
+      itemSelector: '.cert',
+      gutter: 20
+    } );
 });
 
 //typeing effect
